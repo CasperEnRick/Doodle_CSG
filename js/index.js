81ff1050ef0a0118adabfbd9ef094227d5cb6818
casperlamboo <casperlamboo@gmail.com>
2017-09-05 21:51:45 +0200

    simplify code

--- js/index.js
+++ js/index.js
@@ -1,47 +1,9 @@
 var csg = require('csg');
-var { Viewer, addViewer } = require('./viewer');
+var { Viewer } = require('./viewer');
 
-var a = CSG.cube();
-var b = CSG.cylinder({ slices: 8, start: new CSG.Vector(-1, 0, 0), end: new CSG.Vector(1, 0, 0) });
-b = b.union(CSG.cylinder({ slices: 8, start: new CSG.Vector(0, -1, 0), end: new CSG.Vector(0, 1, 0) }));
-b = b.union(CSG.cylinder({ slices: 8, start: new CSG.Vector(0, 0, -1), end: new CSG.Vector(0, 0, 1) }));
-var c = CSG.cube({ radius: 0.5 });
-var d = CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(-1, 0, 0), end: new CSG.Vector(-0.5, 0, 0) });
-d = d.union(CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(0.5, 0, 0), end: new CSG.Vector(1, 0, 0) }));
-d = d.union(CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(0, -1, 0), end: new CSG.Vector(0, -0.5, 0) }));
-d = d.union(CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(0, 0.5, 0), end: new CSG.Vector(0, 1, 0) }));
-d = d.union(CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(0, 0, -1), end: new CSG.Vector(0, 0, -0.5) }));
-d = d.union(CSG.cylinder({ radius: 0.5, slices: 8, start: new CSG.Vector(0, 0, 0.5), end: new CSG.Vector(0, 0, 1) }));
-a.setColor(1, 0, 0);
-b.setColor(0, 0, 1);
-c.setColor(0, 1, 0);
-d.setColor(1, 1, 0);
-var operations = [
-  a,
-  b,
-  a.inverse(),
-  b.inverse(),
-  a.union(b),
-  b.union(a),
-  a.subtract(b),
-  b.subtract(a),
-  a.intersect(b),
-  b.intersect(a),
-  c,
-  d,
-  c.inverse(),
-  d.inverse(),
-  c.union(d),
-  d.union(c),
-  c.subtract(d),
-  d.subtract(c),
-  c.intersect(d),
-  d.intersect(c)
-];
+var cylinder = CSG.cylinder({ slices: 8, start: new CSG.Vector(-1, 0, 0), end: new CSG.Vector(1, 0, 0) });
 
-Viewer.lineOverlay = true;
+// Viewer.lineOverlay = false;
 
-for (var i = 0; i < operations.length; i++) {
-  var viewer = new Viewer(operations[i], 250, 250, 5);
-  document.body.appendChild(viewer.gl.canvas);
-}
+var viewer = new Viewer(cylinder, 720, 480, 10);
+document.getElementById('3d').appendChild(viewer.gl.canvas);
