83980aebc7a3f0866730bd8fa0eb5aeb8a1817e1
Rick Companje <rick@companje.nl>
2017-09-06 08:11:15 +0200

    experiment succeeded, performance almost just as bad

--- js/index.js
+++ js/index.js
@@ -7,8 +7,9 @@ var WIDTH = 720;
 var HEIGHT = 480;
 
 var state = window.state = new State({
-  cylinders: [],
-  eraseCylinders: []
+  // cylinders: [],
+  // eraseCylinders: []
+  csg: new CSG()
 });
 
 var canvas = document.createElement('canvas');
@@ -18,23 +19,7 @@ document.getElementById('d2').appendChild(canvas);
 var context = canvas.getContext('2d');
 
 state.addListener(function(state) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (let i = 0; i < state.cylinders.length; i ++) {
-    context.fillStyle = 'blue';
-    var cylinder = state.cylinders[i];
-    context.beginPath();
-    context.arc(cylinder.x, cylinder.y, cylinder.radius, 0, Math.PI * 2, false);
-    context.fill();
-  }
-
-  for (let i = 0; i < state.eraseCylinders.length; i ++) {
-    var cylinder = state.eraseCylinders[i];
-    context.fillStyle = 'red';
-    context.beginPath();
-    context.arc(cylinder.x, cylinder.y, cylinder.radius, 0, Math.PI * 2, false);
-    context.fill();
-  }
+  // no feedback in 2D yet
 });
 
 
@@ -43,67 +28,35 @@ canvas.onmousemove = function(mouse) {
 
   if (mouse.which & 1) {
     state.update(function(state) {
+
+      //create cylinder
+      var halfWidth = WIDTH / 2;
+      var halfHeight = HEIGHT / 2;
+      var x = mouse.x - halfWidth;
+      var y = 10;
+      var z = mouse.y - halfHeight;
+      var cylinderCSG = CSG.cylinder({
+        slices: 20,
+        start: new CSG.Vector(x, 0, z),
+        end: new CSG.Vector(x, y, z),
+        radius: 20
+      });
+
+      //union or subtract cylinder from csg
       if (mouse.shiftKey) {
-        state.eraseCylinders.push({
-          x: mouse.x,
-          y: mouse.y - offsetTop,
-          radius: 20,
-          height: 10
-        });
+        state.csg = state.csg.subtract(cylinderCSG);
       } else {
-        state.cylinders.push({
-          x: mouse.x,
-          y: mouse.y - offsetTop,
-          radius: 20,
-          height: 20
-        });
+        state.csg = state.csg.union(cylinderCSG);
       }
       return state;
     });
   }
 }
 
-
 var viewer = new Viewer(new CSG(), WIDTH, HEIGHT, 500);
 document.getElementById('d3').appendChild(viewer.gl.canvas);
 
 state.addListener(function(state) {
-  var csg = new CSG();
-
-  var halfWidth = WIDTH / 2;
-  var halfHeight = HEIGHT / 2;
-
-  for (let i = 0; i < state.cylinders.length; i ++) {
-    var cylinderData = state.cylinders[i];
-    var x = cylinderData.x - halfWidth;
-    var y = cylinderData.height;
-    var z = cylinderData.y - halfHeight;
-    var cylinderCSG = CSG.cylinder({
-      slices: 20,
-      start: new CSG.Vector(x, 0, z),
-      end: new CSG.Vector(x, y, z),
-      radius: cylinderData.radius
-    });
-
-    csg = csg.union(cylinderCSG);
-  }
-
-  for (let i = 0; i < state.eraseCylinders.length; i ++) {
-    var cylinderData = state.eraseCylinders[i];
-    var x = cylinderData.x - halfWidth;
-    var y = cylinderData.height;
-
-    var z = cylinderData.y - halfHeight;
-    var cylinderCSG = CSG.cylinder({
-      slices: 20,
-      start: new CSG.Vector(x, 0, z),
-      end: new CSG.Vector(x, y, z),
-      radius: cylinderData.radius
-    });
-
-    csg = csg.subtract(cylinderCSG);
-  }
-
-  viewer.mesh = csg.toMesh();
+  viewer.mesh = state.csg.toMesh();
   viewer.gl.ondraw();
 });
